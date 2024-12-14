60c2f2c06d444b2cc95d38fc8ce7ec4feab0f885
Karen Dantas <karennobre74@gmail.com>
2024-12-14 16:22:15 -0300

    feat: first commit

---
+++ src/components/welcome/styles.ts
@@ -0,0 +1,17 @@
+import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    logo: {
+        width: 48,
+        height: 48,
+        marginTop: 24,
+        marginBottom: 28
+    },
+    title: {
+        fontSize: 24,
+        fontFamily: fontFamily.bold,
+        color: colors.gray[600]
+    }
+})
